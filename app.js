a5707bd4f5ce92959f275ca89efb4311f9a66f8a
Sattorov Kamronbek <PubgGamerUzbSila@gmail.com>
2023-07-27 12:37:29 +0500

    the fja;lkjd

--- app.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector("#btn");
-const btnText = document.querySelector("#btnText");
-
-btn.onclick = () => {
-    btnText.innerHTML = "Thanks";
-    btn.classList.add("active");
-};
---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector("#btn");
+const btnText = document.querySelector("#btnText");
+const i1 = document.querySelector('#i1')
+const i2 = document.querySelector('#i2')
+const i3 = document.querySelector('#i3')
+
+
+    btn.onclick = () => {
+        if(i1 < 0 && i2 < 0 && i3 < 0){
+        console.log('aslaoska')
+        }else{
+            btnText.innerHTML = "Message Sending!!!";
+            btn.classList.add("active");
+            console.log('dsfd   ')
+        }
+    };
